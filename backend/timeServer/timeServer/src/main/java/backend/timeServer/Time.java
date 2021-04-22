6edac1b40b86800133daa161a0c651404dd69e3d
marcelunk <marcel@unkauf.org>
2021-04-22 10:48:50 +0200

    added Time zone

--- backend/timeServer/timeServer/src/main/java/backend/timeServer/Time.java
+++ backend/timeServer/timeServer/src/main/java/backend/timeServer/Time.java
@@ -1,12 +1,25 @@
 package backend.timeServer;
 
-public class Time {
-    
-    public static String getNewYorkTime() {
+import java.util.TimeZone;
 
+/**
+ * 
+ * @author Marcel Unkauf
+ */
+public class Time {
+
+    public static String getGermanTime(){
+        TimeZone tz = TimeZone.getTimeZone("Germany/Berlin");
+        return tz.getDisplayName();
+    }
+    
+    public static String getNewYorkTime(){
+        TimeZone tz = TimeZone.getTimeZone("America/New_York");
+        return tz.getDisplayName();
     }
 
-    public static String getNewYorkTime() {
-        TimeZone tz = new 
+    public static String addOneHour(){
+        // What time zone is running currently? -> Add one hour
+        return null;
     }
 }

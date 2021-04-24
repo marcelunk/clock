74b868ded130f33c83c8d493d8e0c3528238e241
marcelunk <marcel@unkauf.org>
2021-04-24 17:10:20 +0200

    working seconds

--- frontend/src/components/Clock.js
+++ frontend/src/components/Clock.js
@@ -1,33 +1,42 @@
 import "./Clock.css";
-import React, { useEffect, useState } from 'react';
+import React, {Component} from 'react';
 
-export default function Clock(props) {
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
 
-    const[hours, setHours] = useState(0);
-    const[minutes, setMinutes] = useState(0);
-    const[seconds, setSeconds] = useState(0);
+        this.state = {
+            seconds: 0,
+            minutes: 0,
+            hours: 0
+        };
+    }
 
-    useEffect( () => {
-        setTimeout(() => {
-            setSeconds(seconds+1)
-            console.log(seconds)
-            if(seconds == 60) {
-                setSeconds(0)
-                setMinutes(minutes+1)
-                if(minutes == 60) {
-                    setMinutes(minutes%60)
-                    setHours(hours+1)
-                    if(hours == 24) {
-                        setHours(hours%1)
-                    }
-                }
+    componentDidMount() {
+        setInterval(() => {
+            if (this.state.seconds >= 59) {
+                this.setState((prevState) => ({ minutes: prevState.minutes + 1, seconds: 0 }));                
             }
+            if (this.state.minutes >= 59) {
+                this.setState((prevState) => ({ hours: prevState.hours + 1, minutes: 0, seconds: 0 })); 
+            }
+            this.setState((prevState) => ({ seconds: prevState.seconds + 1 }))
         }, 100);
-    }, [hours, minutes, seconds]);
+    }
+    
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
 
-    return (
-        <div className="clock">            
-            {hours}:{minutes}:{seconds}
-        </div>
-    )
+
+
+    render() {
+        return (
+            <div className="clock">            
+                { this.state.hours + ":" + this.state.hours + ":" + this.state.seconds }
+            </div>
+        )
+    }
 }
+
+export default Clock;

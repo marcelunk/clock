b2f44b79591da96e276e289486e8663384752179
marcelunk <marcel@unkauf.org>
2021-04-25 08:00:19 +0200

    making the new server run

--- backend/timeServer/timeServer/src/main/java/backend/timeServer/Time.java
+++ backend/timeServer/timeServer/src/main/java/backend/timeServer/Time.java
@@ -16,8 +16,21 @@ public class Time {
     //America/New_York
     public Time(String timeZone) {
       ZonedDateTime time = ZonedDateTime.now(ZoneId.of(timeZone));
+      // ZonedDateTime time = ZonedDateTime.now(ZoneId.of("Europe/Berlin"));
       this.hour = time.getHour();
       this.minute = time.getMinute();
       this.second = time.getSecond();
+    }
+
+    public int getHour() {
+      return this.hour;
+    }
+
+    public int getMinute() {
+      return this.minute;
+    }
+
+    public int getSecond() {
+      return this.second;
     }
 }
--- backend/timeServer/timeServer/src/main/java/backend/timeServer/TimeController.java
+++ backend/timeServer/timeServer/src/main/java/backend/timeServer/TimeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3000")
+// @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class TimeController {
 
